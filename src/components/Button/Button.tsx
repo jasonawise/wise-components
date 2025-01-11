9516d3e38e54941da5c611a4983869d7a2365ba4
Jason A. Wise <jasonandrewwise@gmail.com>
2025-01-11 12:59:29 -0500

    button width

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,18 @@ import React from "react";
 
 interface ButtonProps {
   children: React.ReactNode;
+  buttonWidth: string;
   handleClick: () => void;
 }
 
-export default function Button({ children, handleClick }: ButtonProps) {
+export default function Button({
+  children,
+  buttonWidth,
+  handleClick,
+}: ButtonProps) {
   return (
     <button
-      className="border border-solid px-4 py-2 rounded"
+      className={`border border-solid px-4 py-2 rounded ${buttonWidth}`}
       onClick={handleClick}
     >
       {children}
